abf225bb89846ddaae27f0a9153ac613836da0cb
valdemirlsf <valdemirlsf@gmail.com>
2021-03-19 22:08:59 -0300

    adição dos arquivos do projeto recriando interface do instagram

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var logado = document.getElementById("logado");
+var login = document.getElementById("nologin");
+var trocarconta = document.getElementById("trocarconta");
+
+function exibelogin(){
+    logado.style.display = 'none';
+    login.style.display = 'flex';
+    trocarconta.style.display = "none";
+}
